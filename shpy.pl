974ca3e8669a2fef1b9dccd1fe73099e8c5a98bb
Nicholas Leung <kai.h.leung@student.unsw.edu.au>
2015-09-16 02:56:49 +1000

    Refer to diary, my code sucks

--- shpy.pl
+++ shpy.pl
@@ -6,6 +6,7 @@
 
 
 $import = 0;
+$identation = 0;
 while ($line = <>) {
     chomp $line;
     if ($line =~ /^#!/ && $. == 1) {
@@ -14,15 +15,19 @@ while ($line = <>) {
         #This should take echo and change it to print, and add quotation marks
         $line =~ s/echo /print "/;
         if ($line =~ /\$+/){
-            $line =~ s/[\$\"]//g;
-            my @words = split / /,$line;
-            shift @words; 
-            $line = "print ".(join(", ", @words));
-            print "$line\n";
+            $line =~ s/print \"//;
+			#print "$line\n";
+			my @vars = split/\$/,$line;
+			shift (@vars);
+			s{^\s+|\s+$}{}g foreach @vars;  #Removes trailing and leading whitespaces
+			#foreach $ele (@vars){
+			#	print "$ele end\n";
+			#}
+			$line = (" "x$identation)."print ".(join(", ", @vars));
         } else {
     		$line = $line . '"';
-	    	print "$line\n";
         }
+		print "$line\n";
 	} elsif ($line =~ /ls|pwd|id|date/){
 		#This should change ls to subprocess. 
         #Changes whatever afterwards to join word by word.
@@ -39,6 +44,24 @@ while ($line = <>) {
         my @words = split /=/,$line;
         $line = join(" = \'", @words)."'";
         print "$line\n";
+	} elsif ($line =~ /for .* in/){
+		my @object = split/in/, $line;
+		s{^\s+|\s+$}{}g foreach @object; #Removes trailing and leading whitespaces
+		$line = shift(@object)." in ";
+		my $tempvar = (shift(@object));
+		print "$tempvar\n";
+		#Have to figure out how to add qutation marks to only words.
+		print "$line\n";
+		#$forloopc += 1;
+	} elsif ($line =~ /^$/){
+		#Fixing Empty line
+		print "\n";
+	} elsif ($line =~ /^do$/){
+		#There will be more added to accomodate other cases just for identation
+		$identation+=4;
+	} elsif ($line =~ /^done$/){
+		#There will be more added to accomodate other cases just for identation
+		$identation-=4;
     } else {
         # Lines we can't translate are turned into comments
         print "#$line\n";
