3bc2bd48f30cee638f4ccea17073e9f3d7931c9c
Nicholas Leung <kai.h.leung@student.unsw.edu.au>
2015-09-20 04:18:04 +1000

    Refer to diary

--- shpy.pl
+++ shpy.pl
@@ -14,36 +14,71 @@ while ($line = <>) {
         print "#!/usr/bin/python2.7 -u\n";
     } elsif ($line =~ /echo/) {
         #This should take echo and change it to print, and add quotation marks
-        $line =~ s/echo /print "/;
+		if ($line =~ /echo "/){
+			$quote = '"';
+		} else {
+			$quote = "'";
+		}
+       
         if ($line =~ /\$+/){            
-			$line =~ s/print "/print /;
+			$line =~ s/echo[\s$quote]/print /;
 			$line =~ s{^\s+|\s+$}{}g;	#Removes trailing and leading whitespaces
 			my @vars = split/ /,$line;
 			shift (@vars);
 			foreach my $ele (@vars){
 		    	if ($ele =~ m/^[^\$]/){
 					$ele = "'".$ele."'";
+				} elsif ($ele =~ m/^\$[0-9]+/) {
+					$import{"sys"} = 1;
+					$ele =~ s/\$/sys.argv[/;
+					$ele = $ele."]";
 				} else {
 					$ele =~ s/\$//;
 				}
 			 }
 			 $line = (" "x$identation)."print ".(join(", ", @vars)); 
-        } else {
-    		$line = $line . '"';
+		} elsif ($line =~ /echo '.*'/){
+			$line =~ s/echo /print /;
+		} elsif ($line =~ /echo ".*"/){
+			$line =~ s/echo /print /;
+		} else {
+			$line =~ s/echo[\s$quote]/print $quote/;	
+    		$line = $line . $quote;
         }
 		push (@translated, $line);
-	} elsif ($line =~ /ls|pwd|id|date/){
+	} elsif ($line =~ /^[\t\s]*ls|^[\t\s]*pwd|^[\t\s]*id|^[\t\s]*date/){
 		#This should change ls to subprocess. 
         #Changes whatever afterwards to join word by word.
         $import{"subprocess"} = 1;
-		my @words = split / /,$line;
-		$line =  "subprocess.call(['";
-		$line = $line.(join( "','", @words))."'])";
+		if ($line =~ m/-las/){
+			$import{"sys"} = 1;
+			if ($line =~ m/"\$@"/){
+				$temparg = "sys.argv[1:]";
+				$line =~ s/"\$@"//;
+			}
+			#print "$line\n";
+			$line =~ s/^\s+|\s+$//g;
+			my @words = split / /,$line;
+			$line =  "subprocess.call(['";
+			$line = $line.(join( "','", @words))."'] + ".$temparg.")";		
+		} else {
+			my @words = split / /,$line;
+			$line =  "subprocess.call(['";
+			$line = $line.(join( "','", @words))."'])";
+		}
 		push (@translated, $line);
-    } elsif ($line =~ /.*=.*/){
+    } elsif ($line =~ /[a-zA-Z0-9]+=.*/){
         #Handle Variable
         my @words = split /=/,$line;
-        $line = join(" = \'", @words)."'";
+		if ($words[1] =~ m/^\$.*/){
+			#means its a variable
+			$import{"sys"} = 1;
+			$words[1] =~ s/\$/sys.argv[/;
+			$words[1] = $words[1]."]";
+			$line = join(" = ", @words);
+		} else {
+	        $line = join(" = \'", @words)."'";
+		}
         push (@translated, $line);
 	} elsif ($line =~ /for .* in/){
 		my @object = split/in/, $line;
@@ -76,10 +111,6 @@ while ($line = <>) {
         $line =~ s/cd /os.chdir('/;
         $line = $line."')";
         push (@translated, $line);
-	} elsif ($line =~ /^[\t\s]*$/){
-		#Fixing Empty line
-		$line = "\n";
-        push (@translated, $line);
     } elsif ($line =~ /^[\t\s]*exit/){
         $import{"sys"} = 1;
         $line =~ s/exit /sys.exit(/;
@@ -92,15 +123,51 @@ while ($line = <>) {
 		#print "$var\n";
 		$line = "$var = sys.stdin.readline().rstrip()";
 		push (@translated, $line);
-	} elsif ($line =~ /^[\t\s]*do$/){
+	} elsif ($line =~ /^if test|^elif test/){
+		#rint "Doing ifs\n";
+		if ($line =~ /^if/){
+			$line =~ s/if test //;
+			$newline = "if ";
+		} elsif ($line =~ /^elif/){
+			$line =~ s/elif test //;
+			$newline = "elif ";
+		}
+		if ($line =~ m/-r/){
+			$import{"os"} = 1;
+			$line =~ s/-r//;
+			$line =~ s{^\s+|\s+$}{}g;
+			$newline = $newline."os.access('".$line."', os.R_OK):"
+		} elsif ($line =~ m/-d/){
+			$import{"os.path"} = 1;
+			$line =~ s/-d//;
+			$line =~ s{^\s+|\s+$}{}g;
+			$newline = $newline."os.path.isdir('".$line."'):"
+		} else {
+			my @vars = split/=/, $line;
+			s{^\s+|\s+$}{}g foreach @vars;
+			foreach $ele (@vars){ 
+				$ele = "'".$ele."'";
+			}
+			$newline = $newline.(join(" == ",@vars)).":";
+		}
+		push (@translated, $newline);
+	} elsif ($line =~ /^[\t\s]*else$/){
+		$line =~ s/else/else:/;
+		push (@translated, $line);
+	} elsif ($line =~ /^#/){
+		push (@translated, $line);
+	} elsif ($line =~ /^[\t\s]*$/){
+		#Fixing Empty line
+		$line = "";
+        push (@translated, $line);} elsif ($line =~ /^[\t\s]*do$|^[\t\s]*then$/){
 		#There will be more added to accomodate other cases just for identation
 		$identation+=4;
-	} elsif ($line =~ /^[\t\s]*done$/){
+	} elsif ($line =~ /^[\t\s]*done$|^[\t\s]*fi$/){
 		#There will be more added to accomodate other cases just for identation
 		$identation-=4;
     } else {
         # Lines we can't translate are turned into comments
-        $line =  "#$line\n";
+        $line =  "#$line";
         push (@translated, $line);
     }
 }
---
+++ subset2test.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+#Used to test subset0
+
+
+foreach my $file (glob "examples/2/*.sh"){
+	system("sh $file > sh.output");
+	my $shfile = $file;
+	$file =~ s/\.sh/\.py/;
+	$file =~ s/examples\/1\///;
+	system("./shpy.pl $shfile > $file");
+	system("python -u $file > py.output");
+	system("diff py.output sh.output && echo success");
+
+}
